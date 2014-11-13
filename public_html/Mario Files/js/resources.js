f6de1a98c113611a6c706654bc5b0426f1c2678b
Anna Simon <anna@rocknrollrobots25.com>
2014-11-13 09:09:01 -0800

    background did some background stuff

---
+++ public_html/Mario Files/js/resources.js
@@ -0,0 +1,30 @@
+game.resources = [
+
+	/* Graphics. 
+	 * @example
+	 * {name: "example", type:"image", src: "data/img/example.png"},
+	 */
+        {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
+
+	/* Atlases 
+	 * @example
+	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
+	 */
+		
+	/* Maps. 
+	 * @example
+	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
+	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+ 	 */
+        {name: "lev 1", type: "tmx", src: "data/map/lev 1.tmx"},
+
+	/* Background music. 
+	 * @example
+	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
+	 */	
+
+	/* Sound effects. 
+	 * @example
+	 * {name: "example_sfx", type: "audio", src: "data/sfx/"}
+	 */
+];
