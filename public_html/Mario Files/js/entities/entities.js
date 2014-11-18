dbf1cee526d9759e7e9e59e38f47f48ad0faa1e2
Anna Simon <anna@rocknrollrobots25.com>
2014-11-17 19:27:29 -0800

    fix FIXED IT WOOT WOOT

--- public_html/Mario Files/js/entities/entities.js
+++ public_html/Mario Files/js/entities/entities.js
@@ -12,14 +12,6 @@ game.PlayerEntity = me.Entity.extend({
             }
        }]);
        
-       
-       settings.image = "mario";
-       settings.spritewidth = "128";  
-       settings.spriteheight = "128";
-       settings.width = 128;
-       settings.height = 128;
-       this._super(me.Entity, "init",[x, y, settings]);
-       
        this.body.setVelocity(5, 0);
    },
    update: function(delta){
